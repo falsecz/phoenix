70ee6ea71303ef60bbd714f8a2a51af95d9a5a71
Vaclav Loffelmann <vaclav.loffelmann@socialbakers.com>
2014-03-31 11:27:13 +0200

    disable test as is distabled on upstream

--- phoenix-core/src/test/java/com/salesforce/phoenix/compile/StatementHintsCompilationTest.java
+++ phoenix-core/src/test/java/com/salesforce/phoenix/compile/StatementHintsCompilationTest.java
@@ -54,6 +54,7 @@ import com.salesforce.phoenix.parse.SQLParser;
 import com.salesforce.phoenix.parse.SelectStatement;
 import com.salesforce.phoenix.query.BaseConnectionlessQueryTest;
 import com.salesforce.phoenix.util.QueryUtil;
+import org.junit.Ignore;
 
 
 /**
@@ -64,7 +65,7 @@ public class StatementHintsCompilationTest extends BaseConnectionlessQueryTest {
     private static StatementContext compileStatement(String query, Scan scan, List<Object> binds) throws SQLException {
         return compileStatement(query, scan, binds, null, null);
     }
-    
+
     private static boolean usingSkipScan(Scan scan) {
         Filter filter = scan.getFilter();
         if (filter instanceof FilterList) {
@@ -102,7 +103,7 @@ public class StatementHintsCompilationTest extends BaseConnectionlessQueryTest {
         String query = "SELECT /*+ SKIP_SCAN */ * FROM atable WHERE entity_id='" + id + "'";
         Scan scan = new Scan();
         List<Object> binds = Collections.emptyList();
-        
+
         compileStatement(query, scan, binds);
         assertTrue("The first filter should be SkipScanFilter.", usingSkipScan(scan));
     }
@@ -113,20 +114,20 @@ public class StatementHintsCompilationTest extends BaseConnectionlessQueryTest {
                 "'000000000000001', '000000000000002', '000000000000003', '000000000000004')";
         Scan scan = new Scan();
         List<Object> binds = Collections.emptyList();
-        
+
         compileStatement(query, scan, binds);
         // Verify that it is not using SkipScanFilter.
         assertFalse("The first filter should not be SkipScanFilter.", usingSkipScan(scan));
     }
-    
-    @Test
+
+	@Ignore @Test
     public void testSelectForceRangeScanForEH() throws Exception {
         Connection conn = DriverManager.getConnection(getUrl());
         conn.createStatement().execute("create table eh (organization_id char(15) not null,parent_id char(15) not null, created_date date not null, entity_history_id char(15) not null constraint pk primary key (organization_id, parent_id, created_date, entity_history_id))");
         ResultSet rs = conn.createStatement().executeQuery("explain select /*+ RANGE_SCAN */ ORGANIZATION_ID, PARENT_ID, CREATED_DATE, ENTITY_HISTORY_ID from eh where ORGANIZATION_ID='111111111111111' and SUBSTR(PARENT_ID, 1, 3) = 'foo' and TO_DATE ('2012-0-1 00:00:00') <= CREATED_DATE and CREATED_DATE <= TO_DATE ('2012-11-31 00:00:00') order by ORGANIZATION_ID, PARENT_ID, CREATED_DATE DESC, ENTITY_HISTORY_ID limit 100");
-        assertEquals("CLIENT PARALLEL 1-WAY RANGE SCAN OVER EH ['111111111111111','foo','2011-12-01 00:00:00.000'] - ['111111111111111','fop','2012-12-01 00:00:00.000']\n" + 
-                "    SERVER FILTER BY (CREATED_DATE >= 2011-11-30 AND CREATED_DATE <= 2012-11-30)\n" + 
-                "    SERVER TOP 100 ROWS SORTED BY [ORGANIZATION_ID, PARENT_ID, CREATED_DATE DESC, ENTITY_HISTORY_ID]\n" + 
+        assertEquals("CLIENT PARALLEL 1-WAY RANGE SCAN OVER EH ['111111111111111','foo','2011-12-01 00:00:00.000'] - ['111111111111111','fop','2012-12-01 00:00:00.000']\n" +
+                " SERVER FILTER BY (CREATED_DATE >= 2011-11-30 AND CREATED_DATE <= 2012-11-30)\n" +
+                " SERVER TOP 100 ROWS SORTED BY [ORGANIZATION_ID, PARENT_ID, CREATED_DATE DESC, ENTITY_HISTORY_ID]\n" +
                 "CLIENT MERGE SORT",QueryUtil.getExplainPlan(rs));
     }
 }
